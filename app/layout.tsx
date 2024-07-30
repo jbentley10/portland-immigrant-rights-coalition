7e563907c3ac291a5cbfeffff873584dbddbd10d
John Bentley <jwbentle@gmail.com>
2024-07-30 14:29:04 -0700

    Add appropriate fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,14 +8,16 @@ import "./globals.css";
 
 // Import dependencies
 import React, { useState } from "react";
-import { Inter } from "next/font/google";
+import { Antonio, Inter } from "next/font/google";
 
 // Import components and utils
 import FloatingActionButton from "../components/floating-action-button";
 import { LocaleContext } from "./locale-provider";
 import { Navigation } from "@/components/navigation";
+import Head from "next/head";
 
 // Declare fonts
+const antonio = Antonio({ subsets: ["latin"] });
 const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({
@@ -27,7 +29,21 @@ export default function RootLayout({
 
   return (
     <html lang='en'>
-      <body className={`${inter.className}`}>
+      <style jsx global>{`
+        h1,
+        h2,
+        h3,
+        h4,
+        a,
+        button {
+          font-family: ${antonio.style.fontFamily};
+        }
+
+        p {
+          font-family: ${inter.style.fontFamily};
+        }
+      `}</style>
+      <body>
         <LocaleContext.Provider value={{ isEnglish, setIsEnglish }}>
           <Navigation />
           {children}
