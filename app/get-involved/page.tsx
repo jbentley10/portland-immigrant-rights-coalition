cc2b3286d96dce3f57791ae53399be10d130aec6
John Bentley <jwbentle@gmail.com>
2024-08-15 17:12:19 -0700

    Add remaining folders and files

---
+++ app/get-involved/page.tsx
@@ -0,0 +1,30 @@
+/**
+ * @file page.tsx
+ */
+// Import components and utils
+import { fetchBlocksBySlug } from "../../lib/contentfulData";
+import Content from "../content";
+
+// Set metadata
+export const metadata = {
+  title: process.env.TITLE,
+  description: process.env.META_DESCRIPTION,
+};
+
+export default async function WhatWeDo() {
+  const blocksEnglish = await fetchBlocksBySlug("get-invovled", "en-US");
+  const blocksSpanish = await fetchBlocksBySlug("get-invovled", "es");
+
+  // Wait for the promises to resolve
+  const [english, spanish] = await Promise.all([blocksEnglish, blocksSpanish]);
+
+  return (
+    <main>
+      <Content
+        key={Math.random()}
+        englishBlocks={english}
+        spanishBlocks={spanish}
+      />
+    </main>
+  );
+}
