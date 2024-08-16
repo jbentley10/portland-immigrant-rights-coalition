dbb364a208042e30ae6acb5bf1115da11c04011e
John Bentley <jwbentle@gmail.com>
2024-08-15 17:06:22 -0700

    finish scaffolding all other pages

---
+++ app/donate/page.tsx
@@ -0,0 +1,30 @@
+/**
+ * @file page.tsx
+ */
+// Import components and utils
+import { fetchBlocksBySlug } from "../../lib/contentfulData";
+import Content from "../content";
+
+// Set metadata
+export const metadata = {
+  title: process.env.TITLE,
+  description: process.env.META_DESCRIPTION,
+};
+
+export default async function WhatWeDo() {
+  const blocksEnglish = await fetchBlocksBySlug("donate", "en-US");
+  const blocksSpanish = await fetchBlocksBySlug("donate", "es");
+
+  // Wait for the promises to resolve
+  const [english, spanish] = await Promise.all([blocksEnglish, blocksSpanish]);
+
+  return (
+    <main>
+      <Content
+        key={Math.random()}
+        englishBlocks={english}
+        spanishBlocks={spanish}
+      />
+    </main>
+  );
+}
